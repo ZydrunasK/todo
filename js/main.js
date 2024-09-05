c01e8b33821b16ea8c00f9d91308505ff2322164
Zydrunask <kniukstaz@gmail.com>
2024-09-05 07:26:50 +0300

    up

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log('IM ALIVE');
+const allTaskDOMs = document.querySelectorAll('.task');
+
+for (const taskDOM of allTaskDOMs) {
+    const moreDOM = taskDOM.querySelector('.more');
+    const moreActionsDOM = taskDOM.querySelector('.more-actions');
+
+    if (moreDOM) {
+        moreDOM.addEventListener('click', () => {
+            moreActionsDOM.dataset.visible = 'true';
+        });
+    }
+}
